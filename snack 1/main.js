3e9f6816027373eac4bb4ba0122f962d9b32b151
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-08 18:20:28 +0100

    primo snack finito credo

---
+++ snack 1/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const listaInvitati = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+console.log(listaInvitati);
+
+const nuovaListaInvitati = listaInvitati.map((oggetto, index)=>{
+    return oggetto = {
+        table: 'Tavolo Vip',
+        nome: oggetto,
+        posto: index + 1
+    };
+});
+
+console.log(nuovaListaInvitati);
