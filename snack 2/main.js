3b6e8fde26d82a68c7afa591b159042eb71e0936
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-08 18:47:29 +0100

    creato array

--- snack 2/main.js
+++ snack 2/main.js
@@ -1,2 +1,39 @@
 'use strict';
 
+const elencoStudenti = [
+    {
+        id: '213',
+        nome: 'Marco della Rovere',
+        'somma voti': 78
+    },
+    {
+        id: '110',
+        nome: 'Paola Cortellessa',
+        'somma voti': 96
+    },
+    {
+        id: '250',
+        nome: 'Andrea Mantegna',
+        'somma voti': 48
+    },
+    {
+        id: '145',
+        nome: 'Gaia Borromini',
+        'somma voti': 74
+    },
+    {
+        id: '196',
+        nome: 'Luigi Grimaldello',
+        'somma voti': 68
+    },
+    {
+        id: '102',
+        nome: 'Piero della Francesca',
+        'somma voti': 50
+    },
+    {
+        id: '120',
+        nome: 'Francesca da Polenta',
+        'somma voti': 84
+    }
+];
