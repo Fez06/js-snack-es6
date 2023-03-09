09cda5feacf855dff75125b96df6cc3c516d5397
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-09 15:01:59 +0100

    readme snack 3

--- snack 3/README.md
+++ snack 3/README.md
@@ -4,3 +4,11 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare in console la bici con peso minore utilizzando destructuring e template literal
 BONUS
 Stampare in pagina oltre che in console!
+
+RISOLUZIONE:
+
+1-Creo un array di oggetti con le proprieta' nome e colore;
+
+2-Ciclo l'array di oggetti mappando il peso;
+
+3-Dal mapping del peso ricavo l'elemento con il peso minore
--- snack 3/main.js
+++ snack 3/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const listaBici = [
+    {
+        nome: 'gialla',
+        peso: '20'
+    },
+    {
+        nome: 'rossa',
+        peso: '25'
+    },
+    {
+        nome: 'nera',
+        peso: '12'
+    },
+    {
+        nome: 'blu',
+        peso: '16'
+    },
+    {
+        nome: 'bianca',
+        peso: '13'
+    },
+    {
+        nome: 'verde',
+        peso: '18'
+    }
+];
+
+const pesiBici = listaBici.map((element)=> element.peso).forEach((element) => element < )
+console.log(pesiBici)
+    
 
 
 
